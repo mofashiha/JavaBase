1de984b997d4641b303351d942405d2706e8de07
zwq <2396172244@qq.com>
2017-03-17 16:31:01 +0800

    this is my first commit
    Signed-off-by: zwq <2396172244@qq.com>

---
+++ JavaBase/src/com/dbd/javacore/Test2.java
@@ -0,0 +1,13 @@
+package com.dbd.javacore;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+	/*
+	 习习与蛤蛤
+	 */
+		System.out.println("习习与蛤蛤！！");
+
+	}
+
+}
---
+++ JavaBase/src/com/dbd/javacore/Test3.java
@@ -0,0 +1,12 @@
+package com.dbd.javacore;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		/**
+		 习习与蛤蛤，我为长者续一秒
+		 */
+		System.out.println("习习与蛤蛤，我为长者续一秒！");
+	}
+
+}
